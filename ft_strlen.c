4f2a9271d07a1390fbdff35eab7a2a90fb27bccc
makhmudego <makhmudego@yandex.ru>
2020-05-01 04:12:42 +0300

    function ft_strlen:

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,9 +6,16 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 03:25:14 by mizola            #+#    #+#             */
-/*   Updated: 2020/05/01 03:26:05 by mizola           ###   ########.fr       */
+/*   Updated: 2020/05/01 04:10:52 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+int		ft_strlen(const char **str)
+{
+	int index;
 
+	index = 0;
+	while (str[index] != '\0')
+		index++;
+	return (index);
+}
