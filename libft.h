64efd2a578fd81481d1961d9da828c1344e9add9
makhmudego <makhmudego@yandex.ru>
2020-05-02 16:43:03 +0300

    libft: upd

--- libft.h
+++ libft.h
@@ -6,12 +6,23 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 19:12:53 by mizola            #+#    #+#             */
-/*   Updated: 2020/05/01 19:13:33 by mizola           ###   ########.fr       */
+/*   Updated: 2020/05/02 16:30:23 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_LIBFT_H
-#define LIBFT_LIBFT_H
-#include <unistd.h>
-#include <string.h>
-#endif //LIBFT_LIBFT_H
+# define LIBFT_LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <strings.h>
+# include <string.h>
+
+size_t		ft_strlen(const char *str);
+void		*ft_memset(void *destination, int c, size_t n);
+void		ft_bzero(void *s, size_t n);
+void		*ft_memcpy(void *destination, const void *source, size_t n);
+void		*ft_memccpy(void *destination, const void *source, int c, size_t n);
+
+#endif
