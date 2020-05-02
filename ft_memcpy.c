92c0d1a9e59d2c6f8a4cee62584215d4e6cddf9b
makhmudego <makhmudego@yandex.ru>
2020-05-03 00:10:03 +0300

    ft_memcpy: upd

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/01 20:48:39 by mizola            #+#    #+#             */
-/*   Updated: 2020/05/01 22:58:29 by mizola           ###   ########.fr       */
+/*   Updated: 2020/05/02 22:46:50 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	*ft_memcpy(void *destination, const void *source, size_t n)
 {
+	size_t i;
+
+	i = 0;
 	if (n == 0 || destination == source)
 		return (destination);
-	while (n)
+	while (i < n)
 	{
-		*((char*)destination + n - 1) = *((char*)source + n - 1);
-		n--;
+		*((char*)destination + i) = *((char*)source + i);
+		i++;
 	}
 	return (destination);
 }
