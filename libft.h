987d71c09c49d444434a5854dded4d202f4ea1f5
makhmudego <makhmudego@yandex.ru>
2020-05-03 03:06:48 +0300

    libft: upd

--- libft.h
+++ libft.h
@@ -26,5 +26,6 @@ void		*ft_memcpy(void *destination, const void *source, size_t n);
 void		*ft_memccpy(void *destination, const void *source, int c, size_t n);
 void		*ft_memmove(void *destination, const void *source, size_t n);
 void		*ft_memchr(const void *arr, int c, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
