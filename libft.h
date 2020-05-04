5575ca159cfa13f7cbced38a7915ebd334402988
makhmudego <makhmudego@yandex.ru>
2020-05-04 04:02:38 +0300

    libft: upd

--- libft.h
+++ libft.h
@@ -31,5 +31,6 @@ char		*ft_strcpy (char *destination, const char *source);
 size_t		ft_strlcpy(char *dst, const char *src, size_t siz);
 size_t		ft_strlcat(char *dst, const char *src, size_t size);
 char		*ft_strchr(const char *s, int c);
+char		*ft_strrchr(const char *s, int c);
 
 #endif
