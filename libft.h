f058417df145f5b0e7d60edbde4d461d66cafcb7
makhmudego <makhmudego@yandex.ru>
2020-05-04 19:50:28 +0300

    libft: upd

--- libft.h
+++ libft.h
@@ -34,5 +34,6 @@ char		*ft_strchr(const char *s, int c);
 char		*ft_strrchr(const char *s, int c);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int			ft_strncmp(const char *str1, const char *str2, size_t n);
+int			ft_atoi(const char *str);
 
 #endif
