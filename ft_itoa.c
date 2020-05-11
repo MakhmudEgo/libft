a06fe793821b677be7136cb18c8653f86f11477a
makhmudego <makhmudego@yandex.ru>
2020-05-11 23:13:57 +0300

    ft_itoa: ok

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/05/11 14:44:26 by mizola            #+#    #+#             */
-/*   Updated: 2020/05/11 15:20:46 by mizola           ###   ########.fr       */
+/*   Updated: 2020/05/11 23:13:15 by mizola           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,7 @@
 
 static int	num_len(int n)
 {
-	int			i;
-	int			j;
+	int				i;
 	long long int	tmp;
 
 	i = 0;
@@ -25,57 +24,74 @@ static int	num_len(int n)
 		i++;
 		tmp *= (-1);
 	}
-	if (tmp < 10 && n > (-10))
-		return (i + 2);
 	while (tmp >= 10)
 	{
-		j = tmp % 10;
-		tmp /=10;
+		tmp /= 10;
 		i++;
 	}
 	return (i + 2);
 }
 
-static void	push_char(char *s, long long int n)
+static void	push_char(char *s, int n)
 {
-	long long int res;
-	char tmp;
-	int index;
+	long long int	res;
+	int				signed_n;
 
 	res = n;
-	index = 0;
+	signed_n = 0;
 	if (n < 0)
 	{
-		s[index] = '-';
-		index++;
 		res *= (-1);
-		push_char(s, res);
+		signed_n = 1;
 	}
-	else
+	while (res)
 	{
-		if (res > 10)
-			push_char(s, res / 10);
-		tmp = res % 10 + '0';
-		s[index] = tmp;
-		++index;
+		*s = res % 10 + '0';
+		res /= 10;
+		s++;
 	}
-//	s[index] = '\0';
+	if (signed_n)
+	{
+		*s = '-';
+		s++;
+	}
+	*s = '\0';
+}
+
+static void	str_reverse(char *str, int i)
+{
+	char	tmp;
+	int		n;
+	int		j;
+
+	n = 0;
+	j = i - 2;
+	while (n < (i - 1) / 2)
+	{
+		tmp = str[j];
+		str[j] = str[n];
+		str[n] = tmp;
+		n++;
+		j--;
+	}
 }
 
 char		*ft_itoa(int n)
 {
-	char 	*res;
+	char	*res;
+	int		i;
 
-	res = malloc(sizeof(char) * num_len(n));
+	i = num_len(n);
+	res = malloc(sizeof(char) * i);
 	if (res == NULL)
 		return (NULL);
+	if (n == 0)
+	{
+		res[0] = '0';
+		res[1] = '\0';
+		return (res);
+	}
 	push_char(res, n);
+	str_reverse(res, i);
 	return (res);
-}
-
-int 		main(void)
-{
-//	ft_itoa(1234);
-	printf("%s\n", ft_itoa(123));
-//	printf("%s\n", s);
 }
