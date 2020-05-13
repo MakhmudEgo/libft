8fbef60f01b11f47d04bfc477f2b7a7864b417f9
makhmudego <makhmudego@yandex.ru>
2020-05-13 22:08:40 +0300

    libft: upd

--- libft.h
+++ libft.h
@@ -19,6 +19,12 @@
 # include <strings.h>
 # include <string.h>
 
+typedef struct		s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
 size_t		ft_strlen(const char *str);
 void		*ft_memset(void *destination, int c, size_t n);
 void		ft_bzero(void *s, size_t n);
@@ -53,5 +59,6 @@ void		ft_putchar_fd(char c, int fd);
 void		ft_putstr_fd(char *s, int fd);
 void		ft_putendl_fd(char *s, int fd);
 void		ft_putnbr_fd(int n, int fd);
+t_list		*ft_lstnew(void *content);
 
 #endif
