fdb597d1e991b2833ea30605397dd5c36cefd1ee
makhmudego <makhmudego@yandex.ru>
2020-05-14 17:58:26 +0300

    ft_lstmap: ok

---
+++ ft_lstmap.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mizola <mizola@student.21-school.ru>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/05/14 09:06:34 by mizola            #+#    #+#             */
+/*   Updated: 2020/05/14 09:06:35 by mizola           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list *res;
+	t_list *new_lst;
+
+	res = NULL;
+	if (lst)
+	{
+		while (lst)
+		{
+			new_lst = ft_lstnew((*f)(lst->content));
+			if (!new_lst)
+			{
+				ft_lstclear(&res, del);
+				return (res);
+			}
+			ft_lstadd_back(&res, new_lst);
+			lst = lst->next;
+		}
+	}
+	return (res);
+}
